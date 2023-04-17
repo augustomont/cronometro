1d979a9b94bc237ac0feb9c1699ba2e1b55fb52b
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-17 16:32:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let iniciar = document.getElementById("iniciar");
 let pausar = document.getElementById("pausar");
 let zerar = document.getElementById("zerar");
 let resultado = document.getElementById("resultado");
+let milissegundos = 0;
 let segundos = 0;
 let minutos = 0;
 let horas = 0;
@@ -10,17 +11,21 @@ let intervalo;
 
 function iniciarCronometro() {
 	intervalo = setInterval(function() {
-		segundos++;
-		if (segundos == 60) {
-			segundos = 0;
-			minutos++;
-			if (minutos == 60) {
-				minutos = 0;
-				horas++;
+		milissegundos++;
+		if (milissegundos == 100) {
+			milissegundos = 0;
+			segundos++;
+			if (segundos == 60) {
+				segundos = 0;
+				minutos++;
+				if (minutos == 60) {
+					minutos = 0;
+					horas++;
+				}
 			}
 		}
-		cronometro.innerHTML = formatarTempo(horas) + ":" + formatarTempo(minutos) + ":" + formatarTempo(segundos);
-	}, 1000);
+		cronometro.innerHTML = formatarTempo(horas) + ":" + formatarTempo(minutos) + ":" + formatarTempo(segundos) + ":" + formatarMilissegundos(milissegundos);
+	}, 10);
 }
 
 function pausarCronometro() {
@@ -39,6 +44,16 @@ function zerarCronometro() {
 function formatarTempo(tempo) {
 	if (tempo < 10) {
 		tempo = "0" + tempo;
+	}
+	return tempo;
+}
+
+function formatarMilissegundos(tempo) {
+	if (tempo < 10) {
+		tempo = "0" + tempo;
+	}
+	else if (tempo < 100) {
+		tempo = "" + tempo;
 	}
 	return tempo;
 }
